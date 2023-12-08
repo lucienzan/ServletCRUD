1fc466128c36fb82ea4da20fe069023614dd36d7
TheinZan <changwilliam826@gmail.com>
2023-12-08 10:49:21 +0630

    [develop] #added user update and formatted code

--- src/main/java/CRUD/UserRepository.java
+++ src/main/java/CRUD/UserRepository.java
@@ -11,75 +11,94 @@ import Model.UserModel;
 
 public class UserRepository {
 	Connection con;
-	
-	public List<UserModel> GetAll()
-	{
+
+	public List<UserModel> GetAll() {
 		con = DBConnection.GetDBConnection();
 		String query = "SELECT * FROM employee";
 		List<UserModel> userList = new ArrayList<UserModel>();
-		
+
 		try {
 			Statement stmt = con.createStatement();
 			ResultSet result = stmt.executeQuery(query);
-			while(result.next()) {
+			while (result.next()) {
 				int id = result.getInt("id");
-                String firstName = result.getString("firstName");
-                String lastName = result.getString("lastName");
-                int age = result.getInt("Age");
-                userList.add(new UserModel(id,firstName,lastName,age));
-            }
-			
-		}catch(Exception e) {
-            e.printStackTrace();
+				String firstName = result.getString("firstName");
+				String lastName = result.getString("lastName");
+				int age = result.getInt("Age");
+				userList.add(new UserModel(id, firstName, lastName, age));
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		
+
 		return userList;
 	}
-	
-	public UserModel GetUser(int id)
-	{
+
+	public UserModel GetUser(int id) {
 		con = DBConnection.GetDBConnection();
-		String query = "SELECT * FROM employee WHERE id = " + id;		
+		String query = "SELECT * FROM employee WHERE id = " + id;
 		UserModel model = null;
 		try {
 			Statement stmt = con.createStatement();
 			ResultSet result = stmt.executeQuery(query);
-			while(result.next()) {
+			while (result.next()) {
 				int userId = result.getInt("id");
-                String firstName = result.getString("firstName");
-                String lastName = result.getString("lastName");
-                int age = result.getInt("Age");
-               model = new UserModel(userId,firstName,lastName,age);
-            }
-			
-		}catch(Exception e) {
-            e.printStackTrace();
+				String firstName = result.getString("firstName");
+				String lastName = result.getString("lastName");
+				int age = result.getInt("Age");
+				model = new UserModel(userId, firstName, lastName, age);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		
+
 		return model;
 	}
-	
-	public void Create(UserModel model)
-	{
+
+	public void Create(UserModel model) {
 		System.out.println(model.getFirstName());
 		con = DBConnection.GetDBConnection();
 		String query = "INSERT INTO employee (id,firstName,lastName,age) VALUES (?,?,?,?)";
 		try {
-            PreparedStatement pstm=con.prepareStatement(query);
-			pstm.setInt(1,model.getId());
-			pstm.setString(2,model.getFirstName());
-			pstm.setString(3,model.getLastName());
-			pstm.setInt(4,model.getAge());
+			PreparedStatement pstm = con.prepareStatement(query);
+			pstm.setInt(1, model.getId());
+			pstm.setString(2, model.getFirstName());
+			pstm.setString(3, model.getLastName());
+			pstm.setInt(4, model.getAge());
 			int success = pstm.executeUpdate();
-			if(success != 0) {
-				var message = String.format("User's %s is created", model.getFirstName()+ " " + model.getLastName());
+			if (success != 0) {
+				var message = String.format("User's %s is created", model.getFirstName() + " " + model.getLastName());
 				System.out.println(message);
-			}else {
+			} else {
 				System.out.println("Something is wrong.");
 			}
-			
-		}catch(Exception e)
-		{
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void Update(UserModel model) {
+		System.out.println(model.getFirstName());
+		con = DBConnection.GetDBConnection();
+		String query = "UPDATE Employee SET firstName = ? , lastName = ? , age = ? WHERE id = ?";
+		try {
+			PreparedStatement pstm = con.prepareStatement(query);
+			pstm.setString(1, model.getFirstName());
+			pstm.setString(2, model.getLastName());
+			pstm.setInt(3, model.getAge());
+			pstm.setInt(4, model.getId());
+			int success = pstm.executeUpdate();
+			if (success != 0) {
+				var message = String.format("User's %s is updated.", model.getFirstName() + " " + model.getLastName());
+				System.out.println(message);
+			} else {
+				System.out.println("Something is wrong.");
+			}
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
--- src/main/java/Servlets/UserServlet.java
+++ src/main/java/Servlets/UserServlet.java
@@ -1,11 +1,5 @@
 package Servlets;
 
-import jakarta.servlet.RequestDispatcher;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
@@ -13,6 +7,12 @@ import java.util.Random;
 
 import CRUD.UserRepository;
 import Model.UserModel;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 /**
  * Servlet implementation class UserServlet
@@ -20,25 +20,32 @@ import Model.UserModel;
 @WebServlet("/")
 public class UserServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
-	private UserRepository repository;
-	 public void init() {
-	        repository = new UserRepository();
-	    }
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	private UserRepository repository;
+
+	@Override
+	public void init() {
+		repository = new UserRepository();
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		String action = request.getServletPath();
 
 		try {
 			switch (action) {
 			case "/create":
-				CreateForm(request,response);
+				CreateForm(request, response);
 				break;
 			case "/userCreate":
-				SaveUser(request,response);
+				SaveUser(request, response);
 				break;
 			case "/edit":
-				EditForm(request,response);
+				EditForm(request, response);
+				break;
+			case "/userUpdate":
+				UpdateUser(request, response);
 				break;
 			default:
 				GetAll(request, response);
@@ -48,7 +55,7 @@ public class UserServlet extends HttpServlet {
 			throw new ServletException(ex);
 		}
 	}
-	
+
 	private void GetAll(HttpServletRequest request, HttpServletResponse response)
 			throws SQLException, IOException, ServletException {
 		List<UserModel> listUser = repository.GetAll();
@@ -62,7 +69,7 @@ public class UserServlet extends HttpServlet {
 		RequestDispatcher dispatcher = request.getRequestDispatcher("add-edit-form.jsp");
 		dispatcher.forward(request, response);
 	}
-	
+
 	private void EditForm(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		int id = Integer.parseInt(request.getParameter("id"));
@@ -71,52 +78,93 @@ public class UserServlet extends HttpServlet {
 		RequestDispatcher dispatcher = request.getRequestDispatcher("add-edit-form.jsp");
 		dispatcher.forward(request, response);
 	}
-	
+
 	private void SaveUser(HttpServletRequest request, HttpServletResponse response)
-	        throws ServletException, IOException {
-	    Random random = new Random();
-	    int id = random.nextInt();
-	    String firstName = request.getParameter("firstName");
-	    String lastName = request.getParameter("lastName");
-	    String ageParam = request.getParameter("age");
+			throws ServletException, IOException {
+		Random random = new Random();
+		int id = random.nextInt();
+		String firstName = request.getParameter("firstName");
+		String lastName = request.getParameter("lastName");
+		String ageParam = request.getParameter("age");
 
-	    if (firstName == null || firstName.trim().isEmpty()) {
-	        request.setAttribute("firstNameError", "First Name field is required.");
-	    }
+		if (firstName == null || firstName.trim().isEmpty()) {
+			request.setAttribute("firstNameError", "First Name field is required.");
+		}
 
-	    if (lastName == null || lastName.trim().isEmpty()) {
-	        request.setAttribute("lastNameError", "Last Name field is required.");
-	    }
+		if (lastName == null || lastName.trim().isEmpty()) {
+			request.setAttribute("lastNameError", "Last Name field is required.");
+		}
 
-	    int age = 0; // Default value if age parameter is empty
-	    if (!ageParam.isEmpty()) {
-	        try {
-	            age = Integer.parseInt(ageParam);
-	            if (age <= 0 || age > 120) {
-	                throw new NumberFormatException();
-	            }
-	        } catch (NumberFormatException e) {
-	            request.setAttribute("ageError", "Age must be in the range of 1 and 120.");
-	        }
-	    }
+		int age = 0; // Default value if age parameter is empty
+		if (!ageParam.isEmpty()) {
+			try {
+				age = Integer.parseInt(ageParam);
+				if (age <= 0 || age > 120) {
+					throw new NumberFormatException();
+				}
+			} catch (NumberFormatException e) {
+				request.setAttribute("ageError", "Age must be in the range of 1 and 120.");
+			}
+		}
 
-	    // Check for any errors
-	    if (request.getAttribute("firstNameError") != null ||
-	            request.getAttribute("lastNameError") != null ||
-	            request.getAttribute("ageError") != null) {
-	        // If there are errors, redirect back with error messages
+		// Check for any errors
+		if (request.getAttribute("firstNameError") != null || request.getAttribute("lastNameError") != null
+				|| request.getAttribute("ageError") != null) {
+			// If there are errors, redirect back with error messages
 			RequestDispatcher dispatcher = request.getRequestDispatcher("create");
 			dispatcher.forward(request, response);
-	    } else {
-	        UserModel newUser = new UserModel(id, firstName, lastName, age);
-	        repository.Create(newUser);
-	        response.sendRedirect("UserServlet");
-	    }
+		} else {
+			UserModel newUser = new UserModel(id, firstName, lastName, age);
+			repository.Create(newUser);
+			response.sendRedirect("UserServlet");
+		}
 	}
 
+	private void UpdateUser(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		int id = Integer.parseInt(request.getParameter("id"));
+		String firstName = request.getParameter("firstName");
+		String lastName = request.getParameter("lastName");
+		String ageParam = request.getParameter("age");
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		if (firstName == null || firstName.trim().isEmpty()) {
+			request.setAttribute("firstNameError", "First Name field is required.");
+		}
+
+		if (lastName == null || lastName.trim().isEmpty()) {
+			request.setAttribute("lastNameError", "Last Name field is required.");
+		}
+
+		int age = 0; // Default value if age parameter is empty
+		if (!ageParam.isEmpty()) {
+			try {
+				age = Integer.parseInt(ageParam);
+				if (age <= 0 || age > 120) {
+					throw new NumberFormatException();
+				}
+			} catch (NumberFormatException e) {
+				request.setAttribute("ageError", "Age must be in the range of 1 and 120.");
+			}
+		}
+
+		// Check for any errors
+		if (request.getAttribute("firstNameError") != null || request.getAttribute("lastNameError") != null
+				|| request.getAttribute("ageError") != null) {
+			// If there are errors, redirect back with error messages
+			RequestDispatcher dispatcher = request.getRequestDispatcher("edit");
+			dispatcher.forward(request, response);
+		} else {
+			UserModel newUser = new UserModel(id, firstName, lastName, age);
+			repository.Update(newUser);
+			response.sendRedirect("UserServlet");
+		}
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
+		System.out.println();
 	}
 }
